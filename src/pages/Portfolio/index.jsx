b3464bdf6fe1599d70608e1d241012d1f0d035ad
Felipe Pereira <104787971+Felipesv1@users.noreply.github.com>
2022-10-21 15:07:49 -0300

    2 projetos removidos

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -5,8 +5,6 @@ import Carousel from 'nuka-carousel'
 import Recipes from './img/recipesImg.png'
 import Lipeflix from './img/LipeflixImg.png'
 import Portflow from './img/PortflowImg.png'
-import HarryPage from './img/HarryPage.png'
-import Dogs from './img/Dogs.png'
 import Rockepay from './img/Rockepay.png'
 import Todolist from './img/TodoList.png'
 import lol from './img/lol.png'
@@ -31,8 +29,6 @@ export default function Portfolio(){
             <a href="https://recipes-page.netlify.app/" target="_blank" rel="noopener noreferrer"> <img src={Recipes} alt='Imagem recipes' /></a>
             <a href="https://felipesv1.github.io/Lipeflix/" target="_blank" rel="noopener noreferrer"><img src={Lipeflix} alt='Imagem Lipeflix' /></a>
             <a href="https://felipesv1.github.io/Portflow-Desafio-final/" target="_blank" rel="noopener noreferrer"> <img src={Portflow} alt='Imagem Portflow' /></a>
-            <a href="https://hogwartspage.netlify.app/" target="_blank" rel="noopener noreferrer"> <img src={HarryPage} alt='Imagem do  site harrypage' /></a>
-            <a href="https://czjgph.csb.app/dogpage" target="_blank" rel="noopener noreferrer"> <img src={Dogs} alt='Imagem do site do dog' /></a>
             <a href="https://explorer-felipesv1.vercel.app" target="_blank" rel="noopener noreferrer"> <img src={Rockepay} alt='Imagem rockepay' /></a>
             <a href="https://felipesv1.github.io/ToDoApp/" target="_blank" rel="noopener noreferrer"> <img src={Todolist} alt='Imagem de um TodoList' /></a>
             <a href="https://felipesv1.github.io/Pagina-inicial-lol/" target="_blank" rel="noopener noreferrer"> <img src={lol} alt='Imagem do site riot game' /></a>
