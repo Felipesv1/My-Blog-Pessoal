324a9048a9906f70a7fda5550552edee3e1a22ec
Felipe Pereira <104787971+Felipesv1@users.noreply.github.com>
2022-10-21 15:01:20 -0300

    mais projetos adicionados

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -7,6 +7,10 @@ import Lipeflix from './img/LipeflixImg.png'
 import Portflow from './img/PortflowImg.png'
 import HarryPage from './img/HarryPage.png'
 import Dogs from './img/Dogs.png'
+import Rockepay from './img/Rockepay.png'
+import Todolist from './img/TodoList.png'
+import lol from './img/lol.png'
+
 
 export default function Portfolio(){
 
@@ -21,17 +25,20 @@ export default function Portfolio(){
     nextButtonText: '>',
     prevButtonText: '<',
       pagingDotsStyle: {
-    fill: 'none'
+    fill: 'none'    
   }
 }} enableKeyboardControls={true} wrapAround={true}  slidesToShow={2.1}>
             <a href="https://recipes-page.netlify.app/" target="_blank" rel="noopener noreferrer"> <img src={Recipes} alt='Imagem recipes' /></a>
             <a href="https://felipesv1.github.io/Lipeflix/" target="_blank" rel="noopener noreferrer"><img src={Lipeflix} alt='Imagem Lipeflix' /></a>
             <a href="https://felipesv1.github.io/Portflow-Desafio-final/" target="_blank" rel="noopener noreferrer"> <img src={Portflow} alt='Imagem Portflow' /></a>
-            <a href="https://hogwartspage.netlify.app/" target="_blank" rel="noopener noreferrer"> <img src={HarryPage} alt='Imagem recipes' /></a>
-            <a href="https://czjgph.csb.app/dogpage" target="_blank" rel="noopener noreferrer"> <img src={Dogs} alt='Imagem recipes' /></a>
+            <a href="https://hogwartspage.netlify.app/" target="_blank" rel="noopener noreferrer"> <img src={HarryPage} alt='Imagem do  site harrypage' /></a>
+            <a href="https://czjgph.csb.app/dogpage" target="_blank" rel="noopener noreferrer"> <img src={Dogs} alt='Imagem do site do dog' /></a>
+            <a href="https://explorer-felipesv1.vercel.app" target="_blank" rel="noopener noreferrer"> <img src={Rockepay} alt='Imagem rockepay' /></a>
+            <a href="https://felipesv1.github.io/ToDoApp/" target="_blank" rel="noopener noreferrer"> <img src={Todolist} alt='Imagem de um TodoList' /></a>
+            <a href="https://felipesv1.github.io/Pagina-inicial-lol/" target="_blank" rel="noopener noreferrer"> <img src={lol} alt='Imagem do site riot game' /></a>
         </Carousel>
         </S.Projects>
         </S.BoxOne>
        </S.Container>
-    )
+    ) 
 }
