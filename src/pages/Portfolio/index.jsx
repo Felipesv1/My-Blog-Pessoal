32d9ead8c3bb51b542c72dbf3f0fd2af4e686eff
Felipe Pereira <fepereirasilva4@gmail.com>
2022-11-18 16:36:24 -0300

    substitui um link e uma foto do portfolio

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../Header/Index'
 import * as S from './Style'
 import Recipes from './img/recipesImg.png'
-import Lipeflix from './img/LipeflixImg.png'
+import Lipeflix from './img/lipeflix.png'
 import Portflow from './img/PortflowImg.png'
 import Rockepay from './img/Rockepay.png'
 import Todolist from './img/TodoList.png'
@@ -21,7 +21,7 @@ export default function Portfolio(){
           <p>clique na imagem e veja o Projeto</p>
         <S.Projects>
             <a href="https://recipes-page.netlify.app/" target="_blank" rel="noopener noreferrer"> <img src={Recipes} alt='Imagem recipes' /></a>
-            <a href="https://felipesv1.github.io/Lipeflix/" target="_blank" rel="noopener noreferrer"><img src={Lipeflix} alt='Imagem Lipeflix' /></a>
+            <a href="http://lipeflix-app.vercel.app/" target="_blank" rel="noopener noreferrer"><img src={Lipeflix} alt='Imagem Lipeflix' /></a>
             <a href="https://felipesv1.github.io/Portflow-Desafio-final/" target="_blank" rel="noopener noreferrer"> <img src={Portflow} alt='Imagem Portflow' /></a>
             <a href="https://explorer-felipesv1.vercel.app" target="_blank" rel="noopener noreferrer"> <img src={Rockepay} alt='Imagem rockepay' /></a>
             <a href="https://felipesv1.github.io/ToDoApp/" target="_blank" rel="noopener noreferrer"> <img src={Todolist} alt='Imagem de um TodoList' /></a>
