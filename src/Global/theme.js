25224ba12705e208a335475a0e038e7daebb0366
Felipesv1 <fepereirasilva4@gmail.com>
2025-01-11 13:33:14 -0300

    mudanças nas cores e comentarios removidos

--- src/Global/theme.js
+++ src/Global/theme.js
@@ -4,18 +4,18 @@ export const lightTheme = {
   Containers: "#F8F8F8",
   BoxAbout: "#EDEDED",
   BoxOne: "#ECECEC",
+  BoxIcons: "#F8F8F8",
   light: "#2890d6",
   dark:"#262626"
 };
 
 export const darkTheme = {
-  body: "#141414", // Preto suave para minimizar o contraste no dark mode
-  fontColor: "#ECECEC", // Fonte em tom de cinza claro
-  Containers: "#1F1F1F", // Tom mais claro para destacar os contêineres
-  BoxAbout: "#262626", // Cinza médio para seções
-  BoxOne: "#262626",
-  BoxIconsOne: "#262626",
-  BoxIconsTwo: "#262626",
+  body: "#141414",
+  fontColor: "#ECECEC", 
+  Containers: "#1F1F1F",
+  BoxAbout: "#262626", 
+  BoxOne: "#10423d",
+  BoxIcons: "#10423d",
   BoxPs: "#262626",
   dark: "#ffffff",
 };
