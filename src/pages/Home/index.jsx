0bd526321e1e195d537c03e4bb7009f2dab472a2
Felipesv1 <fepereirasilva4@gmail.com>
2025-01-14 23:07:56 -0300

    new icon and change duration animation

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,6 +9,7 @@ import iconCss from "./img/iconCss3.png";
 import iconJs from "./img/iconJS.png";
 import iconReact from "./img/IconReact.png";
 import iconTypescript from "./img/icons8-typescript-48.png";
+import iconC from "./img/iconC.png";
 import { motion } from "motion/react";
 import * as S from "./Style";
 
@@ -20,33 +21,33 @@ export default function Home() {
       <motion.div
         animate={{ opacity: 1 }}
         initial={{ opacity: 0 }}
-        transition={{ duration: 1 , delay: 0.2}}
+        transition={{ duration: 1, delay: 0.2 }}
       >
         <S.BoxGreeting>
           <S.ImgProfile src={Foto} alt="foto" />
           <S.BoxWelcomeTitles>
-            <S.Title>Hi, I am felipe Pereira !!</S.Title>
-            <S.TitleTwo>Welcome to my blog !!</S.TitleTwo>
+            <S.Title>
+              Hello &#x1F44B;, <span>I'm a full-Stack developer</span>{" "}
+            </S.Title>
           </S.BoxWelcomeTitles>
         </S.BoxGreeting>
       </motion.div>
       <motion.div
         animate={{ opacity: 1 }}
         initial={{ opacity: 0 }}
-        transition={{ duration: 1, delay:0.8 }}
-        
+        transition={{ duration: 1, delay: 0.8 }}
       >
         <S.BoxTitle>
-          <h2>Developer Full-Stack</h2>
+          <h2>I build things for web</h2>
         </S.BoxTitle>
       </motion.div>
 
-        <motion.div
-          animate={{ opacity: 1 }}
-          initial={{ opacity: 0 }}
-          transition={{ duration: 1, delay: 1 }}
-        >
-      <S.BoxInfos>
+      <motion.div
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+        transition={{ duration: 1, delay: 1 }}
+      >
+        <S.BoxInfos>
           <S.BoxIcons>
             <S.BoxTitleInfos>
               <h2>Networks</h2>
@@ -66,45 +67,49 @@ export default function Home() {
               </a>
             </S.BoxLinks>
           </S.BoxIcons>
-   
-        <S.BoxIcons>
-          <S.BoxTitleInfos>
-            <h2>Languages</h2>
-          </S.BoxTitleInfos>
-          <S.BoxLinks>
-            <a
-              href="https://developer.mozilla.org/pt-BR/docs/Web/HTML"
-              target="_black"
-            >
-              <S.imgIcons src={iconHtml5} alt="iconHtml5" />
-            </a>
-            <a
-              href="https://developer.mozilla.org/pt-BR/docs/Web/css"
-              target="_black"
-            >
-              <S.imgIcons src={iconCss} alt="iconCss" />
-            </a>
-            <a
-              href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
-              target="_black"
-            >
-              <S.imgIcons src={iconJs} alt="iconJs" />
-            </a>
-            <a href="https://www.typescriptlang.org" target="_black">
-              {" "}
-              <S.imgIcons src={iconTypescript} alt="icone do typescript" />
-            </a>
-            <a
-              href="https://pt-br.reactjs.org/docs/getting-started.html"
-              target="_black"
-            >
-              {" "}
-              <S.imgIcons src={iconReact} alt="iconReact" />
-            </a>
-          </S.BoxLinks>
-        </S.BoxIcons>
-      </S.BoxInfos>
-        </motion.div>
+
+          <S.BoxIcons>
+            <S.BoxTitleInfos>
+              <h2>My Skills</h2>
+            </S.BoxTitleInfos>
+            <S.BoxLinks>
+              <a
+                href="https://developer.mozilla.org/pt-BR/docs/Web/HTML"
+                target="_black"
+              >
+                <S.imgIcons src={iconHtml5} alt="iconHtml5" />
+              </a>
+              <a
+                href="https://developer.mozilla.org/pt-BR/docs/Web/css"
+                target="_black"
+              >
+                <S.imgIcons src={iconCss} alt="iconCss" />
+              </a>
+              <a
+                href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
+                target="_black"
+              >
+                <S.imgIcons src={iconJs} alt="iconJs" />
+              </a>
+              <a href="https://www.typescriptlang.org" target="_black">
+                {" "}
+                <S.imgIcons src={iconTypescript} alt="icone do typescript" />
+              </a>
+              <a href="https://react.dev" target="_black">
+                {" "}
+                <S.imgIcons src={iconReact} alt="iconReact" />
+              </a>
+              <a
+                href="https://pt.wikipedia.org/wiki/C_(linguagem_de_programa%C3%A7%C3%A3o)"
+                target="_black"
+              >
+                {" "}
+                <S.imgIcons src={iconC} alt="icon C" />
+              </a>
+            </S.BoxLinks>
+          </S.BoxIcons>
+        </S.BoxInfos>
+      </motion.div>
     </S.ContainerHome>
   );
 }
